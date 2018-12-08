3748e620c1bffe141e04c90e35c734b0dcae9c2c
Wejknis <weja077@gmail.com>
2018-12-08 13:34:37 +0100

    1 2 3 start i to wszystko

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 
+int main(int liczba_slow, char * tablica_slow[]) {
+    int i;
+    double x = atof(tablica_slow[1]);
+    int dokladnosc = 100;
+    double potega_x = x;
+    double silnia = 1.0;
+    double wynik = x;
+
+    for (i = 2; i <= dokladnosc ; i +=2) {
+        potega_x *= (-x * x);
+        silnia *= i * (i + 1);
+        wynik += potega_x / silnia;
+    }
+
+    printf("sin: = %lf", wynik);
 
     return 0;
 }
